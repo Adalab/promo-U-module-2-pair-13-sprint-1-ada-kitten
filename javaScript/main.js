65bbab3398447bfbf0aebf5f9abc7349f64deb16
Ainhoadlhs <ainhoasilveira@gmail.com>
2023-09-06 17:14:34 +0200

    fixed catlist issue

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,27 +2,22 @@
 
 const catsSection = document.querySelector(".js-list");
 
-const kittenOne = document.querySelector('.kittenOne');
-
-kittenOne = `<li class="card">
+const kittenOne = `<li class="card">
 <article>
     <img
-        class="card_img"
-        src="https://dev.adalab.es/gato-siames.webp"
-        alt="gatito"
+    class="card_img"
+    src="https://dev.adalab.es/gato-siames.webp"
+    alt="gatito"
     />
-    <h3 class="card_title">Anastacio</h3>
-    <h4 class="card_race">Siamés</h4>
-    <p class="card_description">
+        <h3 class="card_title">Anastacio</h3>
+        <h4 class="card_race">Siamés</h4>
+        <p class="card_description">
             Porte elegante, su patrón de color tan característico y sus ojos
             de un azul intenso, pero su historia se remonta a Asía al menos
             hace 500 años, donde tuvo su origen muy posiblemente.
     </p>
 </article>
-</li>`; 
-
-kittenOne.innerHTML = 'kittenOne';
-console.log (kittenOne);
+</li>`;
 
 const kittenTwo  = `<li class="card">
     <img
@@ -54,3 +49,6 @@ const kittenThree  = `<li class="card">
             </p>
 </li>`;
 
+catsSection.innerHTML = kittenOne;
+catsSection.innerHTML += kittenTwo;
+catsSection.innerHTML += kittenThree;
