7a801353f0c2c3cfd0d671300a70b9fee5e27942
virchaca <you@example.com>
2023-09-07 11:06:43 +0200

    prueba

--- javaScript/main.js
+++ javaScript/main.js
@@ -54,3 +54,20 @@ catsSection.innerHTML += kittenTwo;
 catsSection.innerHTML += kittenThree;
 
 // += lo que hace es concatenar los elementos para que no se sobreescriban. 
+
+
+const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
+const kittenName1 = 'Anastacio';
+const kittenDesc1 = ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenRace1 = 'Siamés';
+
+
+const kittenImage2 = 'https://dev.adalab.es/sphynx-gato.webp"';
+const kittenName2 = 'Fiona';
+const kittenDesc2 = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño…hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';  
+const kittenRace2 = 'Sphynx';
+
+const kittenImage3 = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenName3 = 'Cielo';
+const kittenDesc3 = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.'   
+const kittenRace3 = 'Main Coon';
