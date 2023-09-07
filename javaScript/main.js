858086aa66c12d1d4503e96ed3f35e4800457bbd
virchaca <you@example.com>
2023-09-07 11:22:22 +0200

    kitten consts

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,7 +2,7 @@
 
 const catsSection = document.querySelector(".js-list");
 
-const kittenOne = `<li class="card">
+/*const kittenOne = `<li class="card">
 <article>
     <img
     class="card_img"
@@ -47,27 +47,34 @@ const kittenThree  = `<li class="card">
             bella mirada se ha convertido en una de sus señas de identidad.
             Sus ojos son grandes y las orejas resultan largas y en punta.
             </p>
-</li>`;
+</li>`; */
 
-catsSection.innerHTML = kittenOne;
+/*catsSection.innerHTML = kittenOne;
 catsSection.innerHTML += kittenTwo;
-catsSection.innerHTML += kittenThree;
+catsSection.innerHTML += kittenThree;*/
 
 // += lo que hace es concatenar los elementos para que no se sobreescriban. 
 
 
-const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
+const kittenImage1 = '<img src = "https://dev.adalab.es/gato-siames.webp">';
 const kittenName1 = 'Anastacio';
 const kittenDesc1 = ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
 const kittenRace1 = 'Siamés';
 
+const kittenOne = kittenImage1 + kittenName1 + kittenDesc1 + kittenRace1;
 
-const kittenImage2 = 'https://dev.adalab.es/sphynx-gato.webp"';
+const kittenImage2 = '<img src = "https://dev.adalab.es/sphynx-gato.webp">';
 const kittenName2 = 'Fiona';
 const kittenDesc2 = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño…hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';  
 const kittenRace2 = 'Sphynx';
 
-const kittenImage3 = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenTwo = kittenImage2 + kittenName2 + kittenDesc2 + kittenRace2;
+
+const kittenImage3 = '<img src = "https://dev.adalab.es/maine-coon-cat.webp">';
 const kittenName3 = 'Cielo';
 const kittenDesc3 = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.'   
 const kittenRace3 = 'Main Coon';
+
+const kittenThree = kittenImage3 + kittenName3 + kittenDesc3 + kittenRace3;
+
+catsSection.innerHTML = kittenOne + kittenTwo + kittenThree;
