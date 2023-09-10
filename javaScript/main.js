30c098966a9414ffcf079cda2c097da0bb43ae0a
virchaca <you@example.com>
2023-09-10 17:40:37 +0200

    friday class

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const catsSection = document.querySelector(".js-list");
-
 /*poner lo primero definidas las variables que nos piden el segundo dia de clase, name, imagen, race, desc, y luego poner las kittenOne, Two y Three*/
 
 const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
@@ -22,11 +20,23 @@ const kittenTwo = kittenImage2 + kittenName2 + kittenDesc2 + kittenRace2;*/
 
 const kittenImage3 = 'https://dev.adalab.es/maine-coon-cat.webp';
 const kittenName3 = 'Cielo';
-const kittenDesc3 = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.'   
-const kittenRace3 = 'Main Coon';
+const kittenDesc3 = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';   
+const kittenRace3 = 'Main Coon'; /**/
 
-/*esto ya no hace falt porque hemos definido cada caracteristica (variable const) dentro de la variable grande, kittenOne, kittenTwo, kittenThree
+/*esto ya no hace falta porque hemos definido cada caracteristica (variable const) dentro de la variable grande, kittenOne, kittenTwo, kittenThree
 const kittenThree = kittenImage3 + kittenName3 + kittenDesc3 + kittenRace3;*/
+
+
+const catsSection = document.querySelector(".js-list");
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const descrSearchText = input_search_desc.value;
+
+/****************para lo de la raza*********************/
+/*en kittenOne, en <h4 class="card_race"> ponemos ${html} (en vez de kittenRace1) </h4>*/
+
+let html = '';
+if (kittenRace1 === "") {html= `Uy que despiste, no sabemos su raza`;} else {html = kittenRace1;}
+
 
 const kittenOne = `<li class="card">
 <article>
@@ -36,10 +46,12 @@ const kittenOne = `<li class="card">
     alt="gatito"
     />
         <h3 class="card_title">${kittenName1.toUpperCase()}</h3>
-        <h4 class="card_race">${kittenRace1}</h4>
+        <h4 class="card_race">${html}</h4>
         <p class="card_description">${kittenDesc1}</p>
 </article>
 </li>`;
+
+
 
 
 const kittenTwo = `<li class="card">
@@ -73,25 +85,23 @@ catsSection.innerHTML += kittenThree;*/
 
 // += lo que hace es concatenar los elementos para que no se sobreescriban. 
 
+/*
+let html = document.querySelector('.card_race');
+if (kittenRace1 === "") {html= `Uy que despiste, no sabemos su raza`;} else {html = kittenRace1;}
 
-catsSection.innerHTML = kittenOne + kittenTwo + kittenThree;
+  if (kittenRace2 === "") {html = `Uy que despiste, no sabemos su raza`;} else {html = kittenRace2;}
 
-const input_search_desc = document.querySelector('.js_in_search_desc');
+  if (kittenRace3.value === '') {html = 'Uy que despiste, no sabemos su raza';} else {html = kittenRace3.value;}
+*/
 
-const descrSearchText = input_search_desc.value;
 
-if( card.classList.contains('hidden')){
-    kittenOne.classList.add('hidden');
+if( kittenDesc1.includes(descrSearchText) ) {catsSection.innerHTML = kittenOne;
 }
+if( kittenDesc2.includes(descrSearchText) ) {catsSection.innerHTML = kittenTwo;
+}
+if( kittenDesc3.includes(descrSearchText) ) {catsSection.innerHTML = kittenThree;
+}
+else {catsSection = null;}
+       
+/*catsSection.innerHTML = kittenOne + kittenTwo + kittenThree;*/
 
-if( kittenDesc1.includes(descrSearchText) ) {
-    kittenOne.classList.remove ('hidden');
-    }
-    
-    if( kittenDesc2.includes(descrSearchText) ) {
-    //Completa el código
-    }
-    
-    if( kittenDesc3.includes(descrSearchText) ) {
-    //Completa el código
-    }
